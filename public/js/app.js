22dd2bca89852dcb7191d933d3855d3c28b0d708
bowsonwu <bowsonwu@gmail.com>
2017-06-22 17:54:48 +0800

    add bootstrap react

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+var switchModule = (name) => {
+
+    ReactDOM.unmountComponentAtNode(document.getElementById('content-box-container'));
+    var module = null;
+    switch (name) {
+        case 's':
+
+            break;
+    
+        default:
+            break;
+    }
+    
+    ReactDOM.render(module), document.getElementById("left-box-container"));
+}
